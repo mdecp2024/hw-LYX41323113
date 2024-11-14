c791b810435bc2eeec22309ac4aa218bfdc41143
LYX41323113 <c0930634160@gmail.com>
2024-11-14 11:05:50 +0000

    提交訊息

---
+++ 41323113.py
@@ -0,0 +1,11 @@
+# 顯示正確的變數命名
+valid_variable = 10
+anotherValidVar = 20
+_var = 30
+totalSum = valid_variable + anotherValidVar
+
+print("Valid variable names:")
+print(valid_variable)
+print(anotherValidVar)
+print(_var)
+print(totalSum)
