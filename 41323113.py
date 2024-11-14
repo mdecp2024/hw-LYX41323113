cd1de482b27850d3265ba31f682d0dbd5c7c6a22
LYX41323113 <c0930634160@gmail.com>
2024-11-14 12:24:19 +0000

    提交訊息

--- 41323113.py
+++ 41323113.py
@@ -1,11 +1,8 @@
-# 顯示正確的變數命名
-valid_variable = 10
-anotherValidVar = 20
-_var = 30
-totalSum = valid_variable + anotherValidVar
-
-print("Valid variable names:")
-print(valid_variable)
-print(anotherValidVar)
-print(_var)
-print(totalSum)
+n = 5  # 總共的行數
+space = '_'
+for i in range(n):  # 對於每一行
+    # 印出空格
+    print(space * (n - i - 1), end='')  # 在每行前面印出空格
+    # 印出星號
+    print('*' * (2 * i + 1))  # 每行印出 2*i + 1 個星號
+    
